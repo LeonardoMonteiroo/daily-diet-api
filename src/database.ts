49cb0cb2bd14777026de6f54a305cb2f2336d58a
LeonardoMonteiroo <92556142+LeonardoMonteiroo@users.noreply.github.com>
2023-06-29 20:11:14 -0300

    initiate database and server

---
+++ src/database.ts
@@ -0,0 +1,10 @@
+import {knex as setupKnex, Knex} from "knex";
+
+export const config: Knex.Config = {
+    client: "sqlite",
+    connection: {
+        filename: "./db/app.db",
+    }
+}
+
+export const knex = setupKnex(config)
