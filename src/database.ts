8299370068e8c9a07d9a9238e7709d87de311dfa
LeonardoMonteiroo <92556142+LeonardoMonteiroo@users.noreply.github.com>
2023-07-10 20:46:05 -0300

    Creating migrations

--- src/database.ts
+++ src/database.ts
@@ -4,6 +4,11 @@ export const config: Knex.Config = {
     client: "sqlite",
     connection: {
         filename: "./db/app.db",
+    },
+    useNullAsDefault: true,
+    migrations: {
+        extension: 'ts',
+        directory: './db/migrations',
     }
 }
 
